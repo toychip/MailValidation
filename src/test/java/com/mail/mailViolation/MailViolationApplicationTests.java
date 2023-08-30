00dcc50a8f9186aac196ef4e0028b166883f0bd4
toychip <dlawnsgud427@naver.com>
2023-08-30 14:55:23 +0900

    Initial commit

---
+++ src/test/java/com/mail/mailViolation/MailViolationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.mailViolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailViolationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

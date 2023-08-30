4340c6369060cb56aeb0e17af033e1937a7ea751
toychip <dlawnsgud427@naver.com>
2023-08-30 15:23:15 +0900

    Feat: ExelController 추가 (#1)

---
+++ src/main/java/com/mail/mailViolation/comtroller/ExelController.java
@@ -0,0 +1,49 @@
+package com.mail.mailViolation.comtroller;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mail.mailViolation.dto.request.FileUploadRequest;
+import com.mail.mailViolation.dto.response.ValidationResponse;
+import com.mail.mailViolation.service.ExelService;
+
+import lombok.RequiredArgsConstructor;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ExelController {
+
+	private final ExelService exelService;
+
+	@GetMapping("/upload")
+	public String getMailForm() {
+//		@ModelAttribute FileUploadRequest form, Model model
+//		model.addAllAttribute("exelForm")
+		return "uploadForm";
+	}
+	
+	@PostMapping("/upload")
+	public String handleFileUpload(FileUploadRequest form) {
+		
+		log.info("-------------------------파일 처리 전 로그");
+
+		// 파일 유효성 검사 및 처리 로직
+        MultipartFile file = null;
+		if (form != null && form.getFile() != null) {
+	            file = form.getFile();
+	        }
+		
+		// 파일이 비어있을 경우 오류메시지 설정 로직 필요
+		log.info("-------------------------서비스 처리 전 로그");
+		List<ValidationResponse> validationResponseList = null;
+		validationResponseList = exelService.processExcelFile(file);
+		return "redirect:/upload";
+	}
+	
+}

68658d03400d58189b0f8b740ea4ea4605b15a76
toychip <dlawnsgud427@naver.com>
2023-08-30 15:10:27 +0900

    Feat: 검증 통과로 가정, dao 생성 (#1)

---
+++ src/main/java/com/mail/mailViolation/dto/ApprovalMailDao.java
@@ -0,0 +1,26 @@
+package com.mail.mailViolation.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ApprovalMailDao {
+	
+	private Integer docNumber;	// 문서 번호
+	private String draftsman;	// 기안자
+	private String dept;		// 소속부서
+	private String title;		// 제목
+	private LocalDateTime ApprovalDate;	// 결재일
+	private String mailTitle;	// 메일 제목
+	private String recipient;	// 받는 사람
+	private String reference;	// 참조
+	private String blockCause;	// 차단사유
+	private String lastApprover;	// 최종 결재
+}

9d5a93e670a3083860770e44c80b00319558300c
toychip <dlawnsgud427@naver.com>
2023-08-30 15:13:58 +0900

    Feat: 검증 통과 후, Response 생성 (#1)

---
+++ src/main/java/com/mail/mailViolation/dto/response/ValidationResponse.java
@@ -0,0 +1,27 @@
+package com.mail.mailViolation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter // model에 담아야하기 때문에 setter 불가피
+@AllArgsConstructor
+@NoArgsConstructor
+public class ValidationResponse {
+	private String docNumber;	// 문서 번호
+	private String draftsman;	// 기안자
+	private String dept;		// 소속부서
+	private String title;		// 제목
+	private LocalDateTime ApprovalDate;	// 결재일
+	private String mailTitle;	// 메일 제목
+	private String recipient;	// 받는 사람
+	private String reference;	// 참조
+	private String blockCause;	// 차단사유
+	private String lastApprover;	// 최종 결재
+	private String result; 		// 적격 여
+	
+}

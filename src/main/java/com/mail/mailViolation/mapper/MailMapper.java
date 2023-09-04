e007474d2e2a32a11b756e70ab6d11335bac98b2
toychip <dlawnsgud427@naver.com>
2023-09-04 13:51:28 +0900

    Fix: DB 연결 오류 수정 (#3)

--- src/main/java/com/mail/mailViolation/mapper/MailMapper.java
+++ src/main/java/com/mail/mailViolation/mapper/MailMapper.java
@@ -1,6 +1,7 @@
-//package com.mail.mailViolation.mapper;
-//import org.apache.ibatis.annotations.Mapper;
-//
-//@Mapper
-//public interface MailMapper {
-//}
+package com.mail.mailViolation.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MailMapper {
+}

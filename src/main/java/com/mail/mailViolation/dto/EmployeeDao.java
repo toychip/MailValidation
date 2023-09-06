642c7342281ae661877692b66d0fcc56b54db7cd
toychip <dlawnsgud427@naver.com>
2023-09-06 14:28:43 +0900

    Feat: DB에 존재하지 않는 기안자일 경우 default값 추가 (#3)

--- src/main/java/com/mail/mailViolation/dto/EmployeeDao.java
+++ src/main/java/com/mail/mailViolation/dto/EmployeeDao.java
@@ -9,11 +9,23 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 public class EmployeeDao {
-    private String empId;
+    private Long empId;
     private String empName;
     private Long deptId;
     private String empEmail;
     private String useYN;
     private LocalDate rgtDttm;
     private String apprReferYn;
+
+    public static EmployeeDao getDefault() {
+        return EmployeeDao.builder()
+                .empId(-1L)
+                .empName("-1")
+                .deptId(-1L)
+                .empEmail("-1")
+                .useYN("-1")
+                .rgtDttm(LocalDate.now())
+                .apprReferYn("-1")
+                .build();
+    }
 }
--- src/main/java/com/mail/mailViolation/service/InitService.java
+++ src/main/java/com/mail/mailViolation/service/InitService.java
@@ -21,26 +21,24 @@ public class InitService {
 	private final MailMapper mapper;
 
 	public ApprovalMailRequest createMailDao(Row row, String strYear) {
-		// ------------------------------------------------------
+
 		log.info("------------------------------------------------------CreateService 호출");
 
-		// 1.0 으로 된 사번 Integer 형으로 변환
-		String strDocNumber = row.getCell(1).toString();
-//		int intDocNumber = (int)Double.parseDouble(strDocNumber);
-//		Integer docNumber = intDocNumber;
 
+		String strDocNumber = row.getCell(1).toString();
 		log.info("------------------------------------------------------draftsman 전호출");
 
 		String draftsman = row.getCell(2).toString();	// 기안자
 		log.info("------------------------------------------------------dept 전호출");
+
 		String dept = row.getCell(3).toString();		// 소속부서
 		log.info("------------------------------------------------------title 전호출");
+
 		String title = row.getCell(4).toString();	// 제목
 		log.info("------------------------------------------------------temp 전호출");
+
 		String temp = row.getCell(5).toString();
-
 		log.info("------------------------------------------------------날짜 추출 =" + temp);
-
 		log.info("------------------------------------------------------날짜 설정 전호출");
 
 		log.info("------------------------------------------------------year =" + strYear);
@@ -63,19 +61,19 @@ public class InitService {
 		// 분
 		int minutes = Integer.valueOf(temp.substring(9, 11));
 		log.info("------------------------------------------------------minutes =" + minutes);
-		
+
 		LocalDateTime ApprovalDate = LocalDateTime.of(year, month, day, hour, minutes);
-		
+
 		log.info("------------------------------------------------------날짜 합치기 전호출");
-		
+
 		String mailTitle = row.getCell(6).toString();	// 메일 제목
 		String recipient = row.getCell(7).toString();	// 받는 사람
 		String reference = row.getCell(8).toString();	// 참조
 		String blockCause = row.getCell(9).toString();	// 차단사유
 		String lastApprover = row.getCell(10).toString();	// 최종 결재
-		
+
 		log.info("------------------------------------------------------생성 전호출");
-		
+
 		ApprovalMailRequest buildApprovalMailRequest = ApprovalMailRequest.builder()
 											.docNumber(strDocNumber)
 											.draftsman(draftsman)
@@ -94,16 +92,31 @@ public class InitService {
 
 	// Y, N 등 여러개의 리스트 반환시 가장 최신 것으로 반환하는 메서드
 	public EmployeeDao getEmp(String name) {
+		log.info("========================InitService.getEmp");
 		List<EmployeeDao> result = mapper.findEmployeeByDraftsmanAndApprReferYn(name);
+		for (EmployeeDao employeeDao : result) {
+			log.info("========================Dao.getEmpName() = " + employeeDao.getEmpName());
+		}
+		log.info("InitService.getEmp.mapper");
 		EmployeeDao latestEmployee = null;
 
+		if (result.isEmpty()) {
+			log.info("========================EmployeeDao가 null임");
+			return EmployeeDao.getDefault();
+		}
+
 		for (EmployeeDao employeeDao : result) {
+
+			log.info("========================InitService.getEmp.employeedaoforeach");
 			char userYN = employeeDao.getUseYN().charAt(0);
 			if(userYN == 'Y'){
+				log.info("========================employeeDao = " + employeeDao.getEmpEmail());
 				return employeeDao;
 			}
 		}
 
+		// DB에 없는 사람이 기안자일 수 없으므로 DB에 없는 경우 생각 안함
+		log.info("========================InitService.getEmp.첫번쨰포문이끝남");
 		for (EmployeeDao employeeDao : result) {
 			if (latestEmployee == null) {
 				latestEmployee = employeeDao;
@@ -113,6 +126,11 @@ public class InitService {
 				latestEmployee = employeeDao;
 			}
 		}
+		log.info("========================InitService.getEmp.return 전");
+		log.info("========================latestEmployee = " + latestEmployee);
+		log.info("========================latestEmployee.getUseYN() = " + latestEmployee.getUseYN());
+		log.info("========================latestEmployee.getEmpName() = " + latestEmployee.getEmpName());
+		log.info("========================latestEmployee.getEmpEmail() = " + latestEmployee.getEmpEmail());
 
 		return latestEmployee;
 	}
@@ -123,8 +141,8 @@ public class InitService {
 		List<EmployeeDao> bosses = mapper.findBossByDeptId(validOverLapDeptId);
 
 		for (EmployeeDao boss : bosses) {
-			System.out.println("InitService.checkApprovalCondition.forloop");
-			System.out.println("------------------------boss = " + boss);
+			log.info("========================InitService.checkApprovalCondition.forloop");
+			log.info("------------------------boss = " + boss);
 			if (boss.getEmpName().equals(approvalMailRequest.getLastApprover()) ||
 					approvalMailRequest.getReference().contains(boss.getEmpName()) ||
 					approvalMailRequest.getReference().contains(boss.getEmpEmail())) {

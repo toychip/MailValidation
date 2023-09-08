75832714c3728e252eb3a5a00965eaa33d5b1716
toychip <dlawnsgud427@naver.com>
2023-09-08 12:42:09 +0900

    Feat: 현재 업로드의 부적격을 결과 return 해야해서 적격 부적격 리스트 분리 (#5)

--- src/main/java/com/mail/mailViolation/comtroller/ExelController.java
+++ src/main/java/com/mail/mailViolation/comtroller/ExelController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import com.mail.mailViolation.dto.EmployeeDao;
+import com.mail.mailViolation.dto.response.ReturnDto;
 import com.mail.mailViolation.service.InitService;
 import com.mail.mailViolation.service.InsertService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,14 +29,15 @@ public class ExelController {
 	private final InsertService insertService;
 
 	@GetMapping("/upload")
-	public String getMailForm() {
-//		@ModelAttribute FileUploadRequest form, Model model
-//		model.addAllAttribute("exelForm")
+	public String getMailForm(Model model) {
+		//		@ModelAttribute FileUploadRequest form, Model model
+		//		model.addAllAttribute("exelForm")
+//		model.getAttribute()
 		return "uploadForm";
 	}
 	
 	@PostMapping("/upload")
-	public String handleFileUpload(FileUploadRequest form) {
+	public String handleFileUpload(FileUploadRequest form, Model model) {
 		
 		log.info("------------------------- 업로드 중");
 
@@ -46,20 +49,39 @@ public class ExelController {
 		
 		// 파일이 비어있을 경우 오류메시지 설정 로직 필요
 //		log.info("-------------------------서비스 처리 전 로그");
-		List<MailResultDao> mailResultDaoList = exelService.processExcelFile(file);
 
-//		for (MailResultDao mailResultDao : mailResultDaoList) {
-//			log.info("\n\n");
-//			log.info("---------- 문서 번호: " + mailResultDao.getDocNumber());
-//			log.info("---------- 메일 기안자: " + mailResultDao.getDraftsman());
-//			log.info("---------- 기안자 부서: " + mailResultDao.getDept());
-//			log.info("---------- 기안자 부서 코드: " + mailResultDao.getDeptId());
-//			log.info("---------- 적격 여부: " + mailResultDao.getResult());
-//			log.info("---------- 최종 결재자: " + mailResultDao.getLastApprover());
-//			log.info("---------- 결재일: " + mailResultDao.getApprovalDate());
-//		}
+
+		ReturnDto returnDto = exelService.processExcelFile(file);
+		List<MailResultDao> conditionOList = returnDto.getConditionOList();
+		List<MailResultDao> conditionXList = returnDto.getConditionXList();
+//		List<MailResultDao> mailResultDaoList = exelService.processExcelFile(file);
+
+		for (MailResultDao mailResultDao : conditionOList) {
+			log.info("\n\n");
+			log.info("---------- conditionOList 문서 번호: " + mailResultDao.getDocNumber());
+			log.info("---------- conditionOList 메일 기안자: " + mailResultDao.getDraftsman());
+			log.info("---------- conditionOList 기안자 부서: " + mailResultDao.getDept());
+			log.info("---------- conditionOList 기안자 부서 코드: " + mailResultDao.getDeptId());
+			log.info("---------- conditionOList 적격 여부: " + mailResultDao.getResult());
+			log.info("---------- conditionOList 최종 결재자: " + mailResultDao.getLastApprover());
+			log.info("---------- conditionOList 결재일: " + mailResultDao.getApprovalDate());
+		}
+
+		for (MailResultDao mailResultDao : conditionXList) {
+			log.info("\n\n");
+			log.info("---------- conditionXList 문서 번호: " + mailResultDao.getDocNumber());
+			log.info("---------- conditionXList 메일 기안자: " + mailResultDao.getDraftsman());
+			log.info("---------- conditionXList 기안자 부서: " + mailResultDao.getDept());
+			log.info("---------- conditionXList 기안자 부서 코드: " + mailResultDao.getDeptId());
+			log.info("---------- conditionXList 적격 여부: " + mailResultDao.getResult());
+			log.info("---------- conditionXList 최종 결재자: " + mailResultDao.getLastApprover());
+			log.info("---------- conditionXList 결재일: " + mailResultDao.getApprovalDate());
+		}
 		
-		insertService.insertData(mailResultDaoList);
+		insertService.insertData(conditionOList);
+		insertService.insertData(conditionXList);
+
+		model.addAttribute("conditionXList", conditionXList);
 		System.out.println("안전하게 저장 성공");
 		return "redirect:/upload";
 	}

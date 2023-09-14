8787da561c57c29c91e4b21382602d3625097b3a
toychip <dlawnsgud427@naver.com>
2023-09-14 10:49:45 +0900

    BREAKING CHANGE: thymleaf -> jsp 초안 (#7)

--- src/main/java/com/mail/mailViolation/comtroller/AdviceController.java
+++ src/main/java/com/mail/mailViolation/comtroller/AdviceController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.thymeleaf.exceptions.TemplateAssertionException;
 
 @ControllerAdvice
 public class AdviceController {

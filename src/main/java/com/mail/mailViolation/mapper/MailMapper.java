ca0e7a91461ae7f934e5567a6233697cf956ebed
toychip <dlawnsgud427@naver.com>
2023-09-21 10:58:40 +0900

    Refac: MailMapper(interface) 주석 추가 (#9)

--- src/main/java/com/mail/mailViolation/mapper/MailMapper.java
+++ src/main/java/com/mail/mailViolation/mapper/MailMapper.java
@@ -10,11 +10,15 @@ import java.util.List;
 @Mapper
 public interface MailMapper {
 
+    // 지정된 draftsman과 ApprReferYn을 기반으로 EmployeeDao 객체 리스트를 반환하는 메서드
     List<EmployeeDao> findEmployeeByDraftsmanAndApprReferYn(@Param("draftsman") String draftsman);
 
+    // 지정된 deptId를 기반으로 상사(EmployeeDao)의 리스트를 반환하는 메서드
     List<EmployeeDao> findBossByDeptId(@Param("deptId") Integer validOverLapDeptId);
 
+    // MailResultDao 객체를 받아서 유효한 결과를 데이터베이스에 삽입하는 메서드
     void insertValidResult(MailResultDao mailResultDao);
 
+    // 유효한 이메일에 대한 결과(MailResultDao 객체 리스트)를 데이터베이스에서 찾아 반환하는 메서드
     List<MailResultDao> findValidEmail();
 }

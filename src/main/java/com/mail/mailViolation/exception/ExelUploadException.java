92311e573d017c85e46a55049be0c69219e91c76
toychip <dlawnsgud427@naver.com>
2023-09-22 13:52:01 +0900

    Refac: Error custom (#11)

---
+++ src/main/java/com/mail/mailViolation/exception/ExelUploadException.java
@@ -0,0 +1,25 @@
+package com.mail.mailViolation.exception;
+
+public class ExelUploadException extends RuntimeException{
+
+
+    public ExelUploadException() {
+        super();
+    }
+
+    public ExelUploadException(String message) {
+        super(message);
+    }
+
+    public ExelUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExelUploadException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ExelUploadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}

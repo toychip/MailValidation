b2be8780582063c39c16f45127ffd05186658cb9
toychip <dlawnsgud427@naver.com>
2023-10-04 09:53:46 +0900

    Feat: ObjectMapper Bean 등록 및 의존성 추가 (#13)

--- src/main/java/com/mail/mailViolation/MailViolationApplication.java
+++ src/main/java/com/mail/mailViolation/MailViolationApplication.java
@@ -1,7 +1,10 @@
 package com.mail.mailViolation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MailViolationApplication {
@@ -10,4 +13,10 @@ public class MailViolationApplication {
 		SpringApplication.run(MailViolationApplication.class, args);
 	}
 
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
+	}
 }
